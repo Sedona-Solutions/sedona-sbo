f95d894af40a1c5adfe322d4fcdbdb815b833a5b
gmeot <gmeot@sedona.fr>
2017-04-20 18:43:38 +0200

    Fix bug about form theme => rename src/Sedona/SBORuntimeBundle/Resources/views/layout/form-theme.html.twig to src/Sedona/SBORuntimeBundle/Resources/views/layout/sbo-form-theme.html.twig

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
  * fixed a bug about macros file calling : rename SBORuntimeBundle:layout:macros.html.twig to SBORuntimeBundle:layout:sbo-macros.html.twig to avoid self-extending
  * be carefull SBORuntimeBundle:layout:macros.html.twig has been deleted. Call SBORuntimeBundle:layout:sbo-macros.html.twig instead of
  * the avanzu base template is now override to change macro calling : SBORuntimeBundle:layout:base-layout.html.twig
+ * in AppKernel, call SBORuntimeBundle before AvanzuBundle to get form theme of sbo instead of avanzu
--- src/Sedona/SBORuntimeBundle/DependencyInjection/SedonaSBORuntimeExtension.php
+++ src/Sedona/SBORuntimeBundle/DependencyInjection/SedonaSBORuntimeExtension.php
@@ -11,17 +11,18 @@
 
 namespace Sedona\SBORuntimeBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SedonaSBORuntimeExtension extends Extension
+class SedonaSBORuntimeExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -33,5 +34,26 @@ class SedonaSBORuntimeExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['TwigBundle'])) {
+            $container->prependExtensionConfig(
+                'twig',
+                array(
+                    'form_theme'    => array(
+                        'SedonaSBORuntimeBundle:layout:sbo-form-theme.html.twig'
+                    )
+                )
+            );
+        }
     }
 }
